cd7346e5224e849ec59bda96f9d4ad71d4358247
Vuduongphuoc <63326488+Vuduongphuoc@users.noreply.github.com>
2024-11-11 14:09:58 +0700

    small change

--- Assets/Scripts/GameObjectToLevelConverter.cs
+++ Assets/Scripts/GameObjectToLevelConverter.cs
@@ -350,8 +350,6 @@ public class GameObjectToLevelConverter : MonoBehaviour
         currentLoadedLevel = levelId;
         // Clear existing GameObjects in levelObject to prepare for new loading
         var layerManager = levelObject.GetComponent<LayerManager>();
-        var baseLevel = levelObject.GetComponent<BaseLevelObject>();
-
         foreach (Transform child in levelObject.transform)
         {
             Destroy(child.gameObject);
@@ -424,7 +422,6 @@ public class GameObjectToLevelConverter : MonoBehaviour
 
         // Instantiate the screw manager
         var screwManager = Instantiate(Resources.Load("Prefabs/ScrewManager"), levelObject.transform) as GameObject;
-        baseLevel.ScrewManager = screwManager.GetComponent<ScrewManager>();
         screwManager.transform.position = Vector3.zero;
         var screwTransform = screwManager.transform;
 
